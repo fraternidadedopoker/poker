17392a1e07ef60af25d3bfc2f4b18776ea1f84fc
RenanBrayner <renanbrayner@gmail.com>
2020-05-16 21:38:02 -0300

    Updates

---
+++ precache-manifest.d58fdb02a6f06f28cade3b262eb648fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c32db14e9b1f791c17e79d79bf6072f2",
+    "url": "/poker/index.html"
+  },
+  {
+    "revision": "e5feb668ff6fb4581b2f",
+    "url": "/poker/static/css/main.42ca736a.chunk.css"
+  },
+  {
+    "revision": "c22e7fb2d150dd8a9519",
+    "url": "/poker/static/js/2.c2932098.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/poker/static/js/2.c2932098.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5feb668ff6fb4581b2f",
+    "url": "/poker/static/js/main.3b61dabe.chunk.js"
+  },
+  {
+    "revision": "d82b3e42e71fcee9fa43",
+    "url": "/poker/static/js/runtime-main.1b373b8b.js"
+  }
+]);
